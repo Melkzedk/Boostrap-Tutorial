bd11d1410f8b2ee1935d140c033cbb3150dfbd7d
melkzedek wafula <melkywafula29@gmail.com>
2026-02-03 14:59:45 +0300

    changes

--- obj/Debug/net10.0/myWebApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/myWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e040ad4ce39f28d5526c35c21a26c305ec229b75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+671a18780123a6e3b24c3d21c40d7bf03f3c897f")]
 [assembly: System.Reflection.AssemblyProductAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
